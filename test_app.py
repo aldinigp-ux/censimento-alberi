864118a31d58b74f5f69710b4a5be084f561011b
aldinigp-ux <aldinigp@gmail.com>
2026-03-11 02:12:05 +0100

    Add files via upload

--- test_app.py
+++ test_app.py
@@ -15,7 +15,7 @@ if not os.path.exists(CARTELLA_FOTO):
     os.makedirs(CARTELLA_FOTO)
 
 conn = st.connection("gsheets", type=GSheetsConnection, ttl=0)
-st.title("🌳 Registro Alberi di Haldin")
+st.markdown("### 🌳 Registro Alberi di Haldin")
 
 loc = get_geolocation()
 
